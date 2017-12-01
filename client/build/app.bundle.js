25be7a86a7fbed2c6d23c67cfbd682a61fb3ba8e
eselopio <eselopio@gmail.com>
2017-12-01 01:07:49 -0300

    client Front ES6 Pizza

---
+++ client/build/app.bundle.js
@@ -0,0 +1,110 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// XMLHttpRequest wrapper using callbacks
+function rs(obj, successHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(obj.method || "GET", obj.url);
+
+    if (obj.headers) {
+        Object.keys(obj.headers).forEach(function (key) {
+            xhr.setRequestHeader(key, obj.headers[key]);
+        });
+    }
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            successHandler(xhr.response);
+        } else {
+            errorHandler(xhr.statusText);
+        }
+    };
+    xhr.onerror = function () {
+        errorHandler(xhr.statusText);
+    };
+    xhr.send(obj.body);
+}
+
+rs({ url: "http://localhost:4000/pizza/ingredients" }, function (data) {
+    var ingredients = JSON.parse(data);
+    var html = "";
+
+    ingredients.forEach(function (ingredient) {
+        html += "<div><img src=\"\" /><div> " + ingredient.name + " <p> id : " + ingredient.id + " </p></div></div>";
+    });
+    document.getElementById("list").innerHTML = html;
+}, function (error) {
+    console.log(error);
+});
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=app.bundle.js.map
